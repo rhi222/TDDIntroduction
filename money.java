c77d617c7a0e275fca316cf0d0db376910a0e8ce
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-04-30 10:26:09 +0900

    一般化するために例を1つ追加

--- money.java
+++ money.java
@@ -2,10 +2,22 @@ class Dollar {
 	Dollar(int amount){
 		this.amount = amount;
 	}
+
 	Dollar times(int multiplier){
 		return new Dollar(amount * multiplier);
 	}
+
 	int amount;
+
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
+	
+	public boolean equals(Object Object) {
+		return true;
+	}
+
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
 		five.times(2);
