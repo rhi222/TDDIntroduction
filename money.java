1b2d98cbcf67a5aa25d093619f0a73548b0747df
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-04-30 09:58:01 +0900

    1章まで完了

--- money.java
+++ money.java
@@ -1,5 +1,14 @@
-public void testMultiplication() {
-	Dollar five = new Dollar(5);
-	five.times(2);
-	assertEquals(10, five.amount);
+class Dollar {
+	Dollar(int amount){
+		this.amount = amount;
+	}
+	void times(int multiplier){
+		amount = amount * multiplier;
+	}
+	int amount;
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
 }
