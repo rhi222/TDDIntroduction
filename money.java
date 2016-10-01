caa88282c07a82a3e5fe42d104af3bd4d73514fe
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-10-01 23:45:28 +0900

    franのテストコードを追加

--- money.java
+++ money.java
@@ -24,4 +24,10 @@ class Dollar {
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
 	}
+
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
 }
